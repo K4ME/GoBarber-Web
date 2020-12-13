de4ff93618b95eda5c6b0cc1c575dc381bfdb0c2
K4ME <guilhermegodencio@gmail.com>
2020-12-13 16:09:51 -0300

    Finalizando listagem de agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,166 +1,242 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react';
-import DayPicker, {DayModifiers} from 'react-day-picker';
+import { isToday, format, parseISO, isAfter } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
-import { FiClock, FiPower } from 'react-icons/fi';
-import {
-  Container,
-  Header,
-  HeaderContent,
-  Profile,
-  Content,
-  Schedule,
-  NextAppointment,
-  Section,
-  Appointment,
-  Calendar
-} from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import * as S from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/Auth';
 import api from '../../services/api';
 
-interface MonthAvailabilityItem{
+interface MonthAvailabilityItem {
   day: number;
-  availability: boolean;
+  available: boolean;
+}
+
+interface Appointment {
+  id: string;
+  date: string;
+  hourFormatted: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
 }
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
+
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [currentMonth, setCurrentMonth] = useState(new Date());
-  const [monthAvailability, setMonthAvailability] = useState<MonthAvailabilityItem[]>([]);
+  const [monthAvailability, setMonthAvailability] = useState<
+    MonthAvailabilityItem[]
+  >([]);
+  const [appointments, setAppointments] = useState<Appointment[]>([]);
 
   const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
-      if (modifiers.available){
-        setSelectedDate(day);
-      }
-    },[],
-  );
+    if (modifiers.available && !modifiers.disabled) {
+      setSelectedDate(day);
+    }
+  }, []);
 
   const handleMonthChange = useCallback((month: Date) => {
-      setCurrentMonth(month);
-    },[],
-  );
+    setCurrentMonth(month);
+  }, []);
 
   useEffect(() => {
-    api.get(`/providers/${user.id}/month-availability`, {
-      params:{
-        year: currentMonth.getFullYear(),
-        month: currentMonth.getMonth() + 1,
-      }
-    }).then(response => {
-      setMonthAvailability(response.data);
-    })
+    api
+      .get(`/providers/${user.id}/month-availability`, {
+        params: {
+          year: currentMonth.getFullYear(),
+          month: currentMonth.getMonth() + 1,
+        },
+      })
+      .then(response => setMonthAvailability(response.data));
   }, [currentMonth, user.id]);
 
-  const disabledDays = useMemo(() => {
-    const dates = monthAvailability
-      .filter(monthDay => monthDay.availability === false)
+  useEffect(() => {
+    api
+      .get<Appointment[]>('/appointments/me', {
+        params: {
+          year: selectedDate.getFullYear(),
+          month: selectedDate.getMonth() + 1,
+          day: selectedDate.getDate(),
+        },
+      })
+      .then(response => {
+        const appointmentsFormatted = response.data.map(appointment => ({
+          ...appointment,
+          hourFormatted: format(parseISO(appointment.date), 'HH:mm'),
+        }));
+
+        setAppointments(appointmentsFormatted);
+      });
+  }, [selectedDate]);
+
+  const disableDays = useMemo(() => {
+    return monthAvailability
+      .filter(monthDay => monthDay.available === false)
       .map(monthDay => {
         const year = currentMonth.getFullYear();
         const month = currentMonth.getMonth();
 
         return new Date(year, month, monthDay.day);
       });
+  }, [currentMonth, monthAvailability]);
 
-      return dates;
-  },[ currentMonth, monthAvailability ]);
+  const selectedDateAsText = useMemo(() => {
+    return format(selectedDate, "'Dia' dd 'de' MMMM", { locale: ptBR });
+  }, [selectedDate]);
+
+  const selectedWeekDay = useMemo(() => {
+    return format(selectedDate, 'cccc', { locale: ptBR });
+  }, [selectedDate]);
+
+  const morningAppointments = useMemo(() => {
+    return appointments.filter(appointment => {
+      return parseISO(appointment.date).getHours() < 12;
+    });
+  }, [appointments]);
+
+  const afternoonAppointments = useMemo(() => {
+    return appointments.filter(appointment => {
+      return parseISO(appointment.date).getHours() >= 12;
+    });
+  }, [appointments]);
+
+  const nextAppointment = useMemo(() => {
+    return appointments.find(appointment =>
+      isAfter(parseISO(appointment.date), new Date()),
+    );
+  }, [appointments]);
 
   return (
-    <Container>
-      <Header>
-        <HeaderContent>
-          <img src={logoImg} alt='GoBarber'/>
+    <S.Container>
+      <S.Header>
+        <S.HeaderContent>
+          <img src={logoImg} alt="Logo GoBarber" />
 
-          <Profile>
-            <img src={ user.avatar_url } alt={ user.name }/>
-
-            <div>
-              <span> Bem-vindo, </span>
-              <strong> { user.name } </strong>
-            </div>
-          </Profile>
-
-          <button type='button' onClick={signOut}>
-            <FiPower/>
-          </button>
-        </HeaderContent>
-      </Header>
-
-      <Content>
-        <Schedule>
-          <h1> Horários agendados</h1>
-          <p>
-            <span> Hoje </span>
-            <span> Dia 06 </span>
-            <span> Segunda-feira </span>
-          </p>
-
-        <NextAppointment>
-          <strong> Atendimento a seguir </strong>
-          <div>
+          <S.HeaderProfile>
             <img
-              src='https://avatars3.githubusercontent.com/u/33429566?s=400&u=c81dfc54c50ed2c2f8399fea7b1df4fefe1ddb64&v=4'
-              alt='Guilherme Macrini'
+              src={
+                user.avatar_url ||
+                'https://api.adorable.io/avatars/56/abott@adorable.io.png'
+              }
+              alt={user.name}
             />
 
-            <strong>Guilherme Macrini</strong>
-            <span>
-              <FiClock />
-              08:00
-            </span>
-          </div>
-        </NextAppointment>
-
-        <Section>
-          <strong>Manhã</strong>
-
-          <Appointment>
-            <span>
-              <FiClock />
-              08:00
-            </span>
-
             <div>
-              <img
-                src='https://avatars3.githubusercontent.com/u/33429566?s=460&u=c81dfc54c50ed2c2f8399fea7b1df4fefe1ddb64&v=4'
-                alt='Guilherme Macrini'
-              />
-
-              <strong>Guilherme Macrini</strong>
+              <span>Bem-vindo,</span>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
-          </Appointment>
+          </S.HeaderProfile>
 
-        </Section>
+          <button type="button" onClick={signOut}>
+            <FiPower size={20} />
+          </button>
+        </S.HeaderContent>
+      </S.Header>
 
-        <Section>
-          <strong>Tarde</strong>
+      <S.Content>
+        <S.Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            {isToday(selectedDate) && <span>Hoje</span>}
+            <span>{selectedDateAsText}</span>
+            <span>{`${selectedWeekDay}-feira`}</span>
+          </p>
 
-          <Appointment>
-            <span>
-              <FiClock />
-              08:00
-            </span>
+          {isToday(selectedDate) && nextAppointment && (
+            <S.NextAppointment>
+              <strong>Atendimento a seguir</strong>
+              <div>
+                <img
+                  src={
+                    nextAppointment.user.avatar_url ||
+                    'https://api.adorable.io/avatars/80/abott@adorable.io.png'
+                  }
+                  alt={nextAppointment.user.name}
+                />
 
-            <div>
-              <img
-                src='https://avatars3.githubusercontent.com/u/33429566?s=460&u=c81dfc54c50ed2c2f8399fea7b1df4fefe1ddb64&v=4'
-                alt='Guilherme Macrini'
-              />
+                <strong>{nextAppointment.user.name}</strong>
+                <span>
+                  <FiClock size={24} />
+                  {nextAppointment.hourFormatted}
+                </span>
+              </div>
+            </S.NextAppointment>
+          )}
 
-              <strong>Guilherme Macrini</strong>
-            </div>
-          </Appointment>
-        </Section>
-        </Schedule>
-        <Calendar>
+          <S.Section>
+            <strong>Manhã</strong>
+
+            {morningAppointments.length === 0 && (
+              <p>Nenhum agendamento neste período</p>
+            )}
+
+            {morningAppointments.map(appointment => (
+              <S.Appointment key={appointment.id}>
+                <span>
+                  <FiClock size={20} />
+                  {appointment.hourFormatted}
+                </span>
+
+                <div>
+                  <img
+                    src={
+                      appointment.user.avatar_url ||
+                      'https://api.adorable.io/avatars/56/abott@adorable.io.png'
+                    }
+                    alt={appointment.user.name}
+                  />
+
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </S.Appointment>
+            ))}
+          </S.Section>
+          <S.Section>
+            <strong>Tarde</strong>
+
+            {afternoonAppointments.length === 0 && (
+              <p>Nenhum agendamento neste período</p>
+            )}
+
+            {afternoonAppointments.map(appointment => (
+              <S.Appointment key={appointment.id}>
+                <span>
+                  <FiClock size={20} />
+                  {appointment.hourFormatted}
+                </span>
+
+                <div>
+                  <img
+                    src={
+                      appointment.user.avatar_url ||
+                      'https://api.adorable.io/avatars/56/abott@adorable.io.png'
+                    }
+                    alt={appointment.user.name}
+                  />
+
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </S.Appointment>
+            ))}
+          </S.Section>
+        </S.Schedule>
+        <S.Calendar>
           <DayPicker
             weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
             fromMonth={new Date()}
-            disabledDays={[{daysOfWeek: [0,6]}, ...disabledDays]}
+            disabledDays={[{ daysOfWeek: [0, 6] }, ...disableDays]}
             modifiers={{
-              available: {daysOfWeek: [1, 2, 3, 4, 5] },
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
             }}
             onMonthChange={handleMonthChange}
             selectedDays={selectedDate}
@@ -180,10 +256,10 @@ const Dashboard: React.FC = () => {
               'Dezembro',
             ]}
           />
-        </Calendar>
-      </Content>
-    </Container>
-  )
+        </S.Calendar>
+      </S.Content>
+    </S.Container>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,5 @@
-import { shade } from 'polished';
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Container = styled.div``;
 
@@ -13,47 +13,43 @@ export const HeaderContent = styled.div`
   margin: 0 auto;
   display: flex;
   align-items: center;
-
   > img {
-    height: 80px
+    height: 80px;
   }
-
   button {
     margin-left: auto;
-    background: transparent;
-    border: 0;
-
+    background: none;
+    border: none;
     svg {
       color: #999591;
-      width: 20px;
-      height: 20px;
     }
   }
 `;
 
-export const Profile = styled.div`
+export const HeaderProfile = styled.div`
   display: flex;
   align-items: center;
   margin-left: 80px;
-
-  img{
+  img {
     width: 56px;
     height: 56px;
     border-radius: 50%;
   }
-
   div {
     display: flex;
     flex-direction: column;
     margin-left: 16px;
     line-height: 24px;
-
     span {
       color: #f4ede8;
     }
-
-    strong {
+    a {
+      text-decoration: none;
       color: #ff9000;
+      transition: opacity 0.3s;
+      &:hover {
+        opacity: 0.8;
+      }
     }
   }
 `;
@@ -67,23 +63,19 @@ export const Content = styled.main`
 export const Schedule = styled.div`
   flex: 1;
   margin-right: 120px;
-
   h1 {
     font-size: 36px;
   }
-
   p {
     margin-top: 8px;
     color: #ff9000;
     display: flex;
     align-items: center;
     font-weight: 500;
-
     span {
       display: flex;
       align-items: center;
     }
-
     span + span::before {
       content: '';
       width: 1px;
@@ -94,16 +86,13 @@ export const Schedule = styled.div`
   }
 `;
 
-
 export const NextAppointment = styled.div`
   margin-top: 64px;
-
-  strong {
+  > strong {
     color: #999591;
     font-size: 20px;
-    font-weight: 400;
+    font-weight: normal;
   }
-
   div {
     background: #3e3b47;
     display: flex;
@@ -112,34 +101,31 @@ export const NextAppointment = styled.div`
     border-radius: 10px;
     margin-top: 24px;
     position: relative;
-
-    &::before{
+    &::before {
+      content: '';
       position: absolute;
       height: 80%;
-      width: 1px;
+      width: 2px;
+      border-radius: 0 10px 10px 0;
       left: 0;
       top: 10%;
-      content: '';
       background: #ff9000;
     }
-
     img {
       width: 80px;
       height: 80px;
       border-radius: 50%;
     }
-
     strong {
       margin-left: 24px;
-      color: #fff;
+      font-size: 24px;
+      color: #f4ede8;
     }
-
     span {
       margin-left: auto;
       display: flex;
       align-items: center;
       color: #999591;
-
       svg {
         color: #ff9000;
         margin-right: 8px;
@@ -150,7 +136,6 @@ export const NextAppointment = styled.div`
 
 export const Section = styled.section`
   margin-top: 48px;
-
   > strong {
     color: #999591;
     font-size: 20px;
@@ -160,28 +145,28 @@ export const Section = styled.section`
     padding-bottom: 16px;
     margin-bottom: 16px;
   }
+  > p {
+    color: #999591;
+  }
 `;
 
 export const Appointment = styled.div`
   display: flex;
   align-items: center;
-
   & + div {
     margin-top: 16px;
   }
-
   span {
     margin-left: auto;
     display: flex;
     align-items: center;
     color: #f4ede8;
-
+    width: 72px;
     svg {
       color: #ff9000;
       margin-right: 8px;
     }
   }
-
   div {
     flex: 1;
     background: #3e3b47;
@@ -190,93 +175,57 @@ export const Appointment = styled.div`
     padding: 16px 24px;
     border-radius: 10px;
     margin-left: 24px;
-
     img {
       width: 56px;
       height: 56px;
       border-radius: 50%;
     }
-
     strong {
       margin-left: 24px;
-      color: #fff;
       font-size: 20px;
+      color: #f4ede8;
     }
   }
 `;
 
 export const Calendar = styled.aside`
   width: 380px;
-
   .DayPicker {
+    background: #28262e;
     border-radius: 10px;
   }
-
   .DayPicker-wrapper {
     padding-bottom: 0;
-    background: #3e3b47;
-    border-radius: 10px;
   }
-
   .DayPicker,
   .DayPicker-Month {
     width: 100%;
   }
-
-  .DayPicker-NavButton {
-    color: #999591 !important;
-  }
-
-  .DayPicker-NavButton--prev {
-    right: auto;
-    left: 1.5em;
-    margin-right: 0;
-  }
-
   .DayPicker-Month {
     border-collapse: separate;
     border-spacing: 8px;
-    margin: 16px 0 0 0;
-    padding: 16px;
-    background-color: #28262e;
-    border-radius: 0 0 10px 10px;
+    margin: 16px;
   }
-
-  .DayPicker-Caption {
-    margin-bottom: 1em;
-    padding: 0 1em;
-    color: #f4ede8;
-
-    > div {
-      text-align: center;
-    }
-  }
-
   .DayPicker-Day {
     width: 40px;
     height: 40px;
   }
-
   .DayPicker-Day--available:not(.DayPicker-Day--outside) {
     background: #3e3b47;
     border-radius: 10px;
     color: #fff;
   }
-
   .DayPicker:not(.DayPicker--interactionDisabled)
     .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
     background: ${shade(0.2, '#3e3b47')};
   }
-
   .DayPicker-Day--today {
     font-weight: normal;
   }
-
   .DayPicker-Day--disabled {
     color: #666360 !important;
     background: transparent !important;
   }
-
   .DayPicker-Day--selected {
     background: #ff9000 !important;
     border-radius: 10px;
